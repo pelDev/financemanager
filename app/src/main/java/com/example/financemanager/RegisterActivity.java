3763bb24f842420f71fbeb0e0d3f7d2f7675fe01
pelDev <pelumip15@gmail.com>
2020-12-17 12:51:53 +0100

    Project Restarted for esa

--- app/src/main/java/com/example/financemanager/RegisterActivity.java
+++ app/src/main/java/com/example/financemanager/RegisterActivity.java
@@ -17,7 +17,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.financemanager.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
