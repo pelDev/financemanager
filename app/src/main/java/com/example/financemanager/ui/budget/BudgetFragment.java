db0649c97a7d9dda2ec19e05cb1ca9680241b86f
pelDev <pelumip15@gmail.com>
2021-01-06 18:42:52 +0100

    Added Chart to Budget Fragment

--- app/src/main/java/com/example/financemanager/ui/budget/BudgetFragment.java
+++ app/src/main/java/com/example/financemanager/ui/budget/BudgetFragment.java
@@ -10,12 +10,11 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavBackStackEntry;
@@ -25,19 +24,26 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.financemanager.BR;
 import com.example.financemanager.MainActivity;
-import com.example.financemanager.MonthPicker;
 import com.example.financemanager.R;
 import com.example.financemanager.database.budget.Budget;
-import com.google.android.material.snackbar.Snackbar;
+import com.example.financemanager.databinding.BudgetFragmentBinding;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.leinardi.android.speeddial.SpeedDialView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BudgetFragment extends Fragment{
 
     private BudgetListAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private BudgetFragmentBinding binding;
     private BudgetViewModel mBudgetViewModel;
 
     public  BudgetFragment() {
@@ -68,7 +74,8 @@ public class BudgetFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.budget_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.budget_fragment, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -112,6 +119,7 @@ public class BudgetFragment extends Fragment{
         mBudgetViewModel = new ViewModelProvider(this,
                 ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()))
                 .get(BudgetViewModel.class);
+        binding.setVariable(BR.budgetViewModel, mBudgetViewModel);
         mAdapter = new BudgetListAdapter(R.layout.item_budget);
         mRecyclerView = getView().findViewById(R.id.list_budgets);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -122,9 +130,34 @@ public class BudgetFragment extends Fragment{
                 new Observer<List<Budget>>() {
                     @Override
                     public void onChanged(List<Budget> budgets) {
-                        mAdapter.setBudgetList(budgets);
+                        if (budgets.size() > 0) {
+                            mAdapter.setBudgetList(budgets);
+                            initializePieChart(budgets);
+                            binding.listBudgets.setVisibility(View.VISIBLE);
+                            binding.emptyViewIncome.setVisibility(View.GONE);
+                        } else {
+                            binding.listBudgets.setVisibility(View.GONE);
+                            binding.emptyViewIncome.setVisibility(View.VISIBLE);
+                            binding.piechart.setVisibility(View.GONE);
+                        }
                     }
                 });
+    }
+
+    private void initializePieChart(List<Budget> budgets) {
+        List<PieEntry> pieEntries = new ArrayList<>();
+        for (int i = 0; i < budgets.size(); i++) {
+            double amount = (double) budgets.get(i).getAmount();
+            String label = budgets.get(i).getCategory();
+            pieEntries.add(new PieEntry((float) amount, label));
+        }
+        binding.piechart.setVisibility(View.VISIBLE);
+        binding.piechart.animateXY(2500, 2500);
+        PieDataSet pieDataSet = new PieDataSet(pieEntries, "Budget");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieData pieData = new PieData(pieDataSet);
+        binding.piechart.setData(pieData);
+        binding.piechart.invalidate();
     }
 
 }
